7d15f12c5d83c87828eb4e38146dd4d2f3e5b8f9
Sergey Igorevich <galyuk_ui@mail.ru>
2017-11-09 20:02:27 +0700

    Что-то пошло не так

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (event){
+	var btn=document.qerySelector("#btn");
+	
+	btn.addEventListener("click", function (ev){
+		alert ("Привет");
+	});
+});
